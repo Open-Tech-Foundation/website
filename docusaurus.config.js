061970e588b8ddd3c51237ec049080db7c6fed09
Thanga Ganapathy <ganapathy888@gmail.com>
2022-03-21 02:58:58 +0530

    Add homepage with a quote

---
+++ docusaurus.config.js
@@ -0,0 +1,63 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "🌍 Open Tech World",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "open-tech-world", // Usually your GitHub org/user name.
+  projectName: "website", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "🌍 Open Tech World",
+        items: [
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/open-tech-world",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Open Tech World. Built with <a href="https://docusaurus.io/">Docusaurus</a>.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
