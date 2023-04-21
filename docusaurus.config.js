42a29400834210e02ada1c1195a202f359b27454
Thanga Ganapathy <ganapathy888@gmail.com>
2023-04-21 14:20:09 +0530

    Add gtag tracking id

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,6 +31,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: 'G-GMFYD0V12G',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
